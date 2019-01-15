b7b309c9f1c5b9b9fd3a1cc9191cc724e75fd5a2
Derreck Mansheim <Derreck.Mansheim@siriuscom.com>
2019-01-14 18:50:12 -0600

    first commit

---
+++ FirstProgram.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Hello
+{
+	class Program
+        {
+	     static void Main(string[] args)
+		{
+			Console.WriteLine("My first c# program from CLI!");
+		 }
+	}
+	
+}
+						}
