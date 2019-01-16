4ad7f1dcf2f3e19440b643a03759708c410455bf
Derreck Mansheim <Derreck.Mansheim@siriuscom.com>
2019-01-15 22:57:21 -0600

    basic list in c#

---
+++ list-tutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+		var names = new List<string> { "Luke", "Father", "One with the force" };
+		foreach (var name in names)
+		{
+		 	Console.WriteLine($"Hello {name.ToUpper()}!");
+		}
+        }
+    }
+}
