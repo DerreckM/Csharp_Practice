04e9615db5aed6658180f6d33e5c02ec1f8a7396
Derreck Mansheim <mansheim23@hotmail.com>
2019-01-15 23:12:26 -0600

    search and sort lists

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -5,14 +5,44 @@ namespace list_tutorial
 {
     class Program
     {
-        static void Main(string[] args)
+	static void Main(string[] args)
         {
-           
-		var names = new List<string> { "Luke", "Father", "One with the force" };
-		foreach (var name in names)
-		{
-		 	Console.WriteLine($"Hello {name.ToUpper()}!");
-		}
+            WorkingWithStrings();
+        }
+
+        public static void WorkingWithStrings()
+        {
+            var names = new List<string> { "Luke", "Father", "Yoda" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine();
+            names.Add("Chewie");
+            names.Add("Leia");
+            names.Remove("Father");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            var index = names.IndexOf("Yoda");
+            Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
